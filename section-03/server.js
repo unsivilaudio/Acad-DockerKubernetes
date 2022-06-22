576ca19b6f866c20fc22b1927459778fea18a403
unsivilaudio <unsivilaudio@gmail.com>
2022-06-22 09:23:08 -0600

    end of section 3

--- section-03/server.js
+++ section-03/server.js
@@ -13,33 +13,50 @@ app.use(express.static('public'));
 app.use('/feedback', express.static('feedback'));
 
 app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'pages', 'feedback.html');
-  res.sendFile(filePath);
+    const filePath = path.join(__dirname, 'pages', 'feedback.html');
+    res.sendFile(filePath);
 });
 
 app.get('/exists', (req, res) => {
-  const filePath = path.join(__dirname, 'pages', 'exists.html');
-  res.sendFile(filePath);
+    const filePath = path.join(__dirname, 'pages', 'exists.html');
+    res.sendFile(filePath);
 });
 
 app.post('/create', async (req, res) => {
-  const title = req.body.title;
-  const content = req.body.text;
+    const title = req.body.title;
+    const content = req.body.text;
 
-  const adjTitle = title.toLowerCase();
+    const adjTitle = title.toLowerCase();
 
-  const tempFilePath = path.join(__dirname, 'temp', adjTitle + '.txt');
-  const finalFilePath = path.join(__dirname, 'feedback', adjTitle + '.txt');
+    const tempDir = path.join(__dirname, 'temp');
+    const feedbackDir = path.join(__dirname, 'feedback');
 
-  await fs.writeFile(tempFilePath, content);
-  exists(finalFilePath, async (exists) => {
-    if (exists) {
-      res.redirect('/exists');
-    } else {
-      await fs.rename(tempFilePath, finalFilePath);
-      res.redirect('/');
-    }
-  });
+    const tempFilePath = path.join(tempDir, adjTitle + '.txt');
+    const finalFilePath = path.join(feedbackDir, adjTitle + '.txt');
+
+    await fs.writeFile(tempFilePath, content);
+
+    exists(finalFilePath, async file => {
+        if (file) {
+            res.redirect('/exists');
+        } else {
+            await fs.rename(tempFilePath, finalFilePath);
+            res.redirect('/');
+        }
+    });
 });
 
-app.listen(80);
+app.listen(80, async () => {
+    try {
+        await fs.mkdir(path.join(__dirname, 'temp'));
+    } catch {
+        console.log('Temp directory exists');
+    }
+
+    try {
+        await fs.mkdir(path.join(__dirname, 'feedback'));
+    } catch {
+        console.log('Feedback directory exists');
+    }
+    console.log('Listening on port 80.');
+});
