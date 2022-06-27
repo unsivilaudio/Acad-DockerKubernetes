e55e94c8aef3b433cec60f6f37376dd2404ffda3
unsivilaudio <unsivilaudio@gmail.com>
2022-06-27 12:33:33 -0600

    missed file

--- kub-deploy/users-api/users-app.js
+++ kub-deploy/users-api/users-app.js
@@ -9,35 +9,36 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 });
 
 app.use(userRoutes);
 
 app.use((err, req, res, next) => {
-  let code = 500;
-  let message = 'Something went wrong.';
-  if (err.code) {
-    code = err.code;
-  }
+    let code = 500;
+    let message = 'Something went wrong.';
+    if (err.code) {
+        code = err.code;
+    }
 
-  if (err.message) {
-    message = err.message;
-  }
-  res.status(code).json({ message: message });
+    if (err.message) {
+        message = err.message;
+    }
+    res.status(code).json({ message: message });
 });
 
 mongoose.connect(
-  process.env.MONGODB_CONNECTION_URI,
-  { useNewUrlParser: true },
-  (err) => {
-    if (err) {
-      console.log('COULD NOT CONNECT TO MONGODB!');
-    } else {
-      app.listen(3000);
+    process.env.MONGODB_CONNECTION_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    err => {
+        if (err) {
+            console.log('COULD NOT CONNECT TO MONGODB!');
+            console.log(err.message);
+        } else {
+            app.listen(3000);
+        }
     }
-  }
 );
